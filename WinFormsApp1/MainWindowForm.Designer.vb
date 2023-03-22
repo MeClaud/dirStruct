24065acec912699235a180ee88b87a3c86d4cecc
Bogdan Claudiu <office.meclaud@gmail.com>
2023-03-22 20:37:27 +0100

    Projektdateien hinzufügen.

---
+++ WinFormsApp1/MainWindowForm.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainWindowForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainWindowForm))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PathBox = New System.Windows.Forms.TextBox()
+        Me.OutputBox = New System.Windows.Forms.CheckedListBox()
+        Me.ExportButton = New System.Windows.Forms.Button()
+        Me.OpenExports = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PathBox
+        '
+        resources.ApplyResources(Me.PathBox, "PathBox")
+        Me.PathBox.Name = "PathBox"
+        '
+        'OutputBox
+        '
+        Me.OutputBox.FormattingEnabled = True
+        resources.ApplyResources(Me.OutputBox, "OutputBox")
+        Me.OutputBox.Name = "OutputBox"
+        '
+        'ExportButton
+        '
+        resources.ApplyResources(Me.ExportButton, "ExportButton")
+        Me.ExportButton.Name = "ExportButton"
+        Me.ExportButton.UseVisualStyleBackColor = True
+        '
+        'OpenExports
+        '
+        resources.ApplyResources(Me.OpenExports, "OpenExports")
+        Me.OpenExports.Name = "OpenExports"
+        Me.OpenExports.UseVisualStyleBackColor = True
+        '
+        'MainWindowForm
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.OpenExports)
+        Me.Controls.Add(Me.ExportButton)
+        Me.Controls.Add(Me.OutputBox)
+        Me.Controls.Add(Me.PathBox)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "MainWindowForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PathBox As TextBox
+    Friend WithEvents OutputBox As CheckedListBox
+    Friend WithEvents ExportButton As Button
+    Friend WithEvents OpenExports As Button
+End Class
---
+++ WinFormsApp1/MainWindowForm.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Imports System.Windows.Forms.VisualStyles
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class MainWindowForm
+    Private ExportDirectory As String = String.Format("{0}\Exports\", Environment.CurrentDirectory)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ChoosePath()
+    End Sub
+    Private Sub LoadContent()
+        For Each foundFolder In My.Computer.FileSystem.GetDirectories(PathBox.Text)
+            OutputBox.Items.Add(My.Computer.FileSystem.GetName(foundFolder), True)
+        Next
+        For Each foundFile In My.Computer.FileSystem.GetFiles(PathBox.Text)
+            OutputBox.Items.Add(My.Computer.FileSystem.GetName(foundFile), True)
+        Next
+    End Sub
+    Private Sub ChoosePath()
+        Dim dialog As New FolderBrowserDialog()
+        dialog.RootFolder = Environment.SpecialFolder.Desktop
+        dialog.SelectedPath = ""
+
+        If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            PathBox.Text = dialog.SelectedPath
+            LoadContent()
+        Else
+            PathBox.Text = ""
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ChoosePath()
+    End Sub
+
+    Private Sub ExportButton_Click(sender As Object, e As EventArgs) Handles ExportButton.Click
+        Dim expFile As System.IO.StreamWriter
+        Dim expFilePath As String = ExportDirectory & Replace(Replace(Replace(DateTime.Now, ":", ""), ".", ""), " ", "_") & ".txt"
+
+        If Not My.Computer.FileSystem.DirectoryExists(ExportDirectory) Then
+            Directory.CreateDirectory(ExportDirectory)
+        End If
+
+        File.Create(expFilePath).Close()
+
+        expFile = My.Computer.FileSystem.OpenTextFileWriter(expFilePath, True)
+        For Each checkedLine In OutputBox.CheckedItems
+            expFile.WriteLine(checkedLine)
+        Next
+        expFile.Close()
+    End Sub
+
+    Private Sub OpenExports_Click(sender As Object, e As EventArgs) Handles OpenExports.Click
+        Process.Start("explorer.exe", ExportDirectory)
+    End Sub
+End Class
